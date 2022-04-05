49b1144e1c501b68dc63f0d348d437f30a3df523
cyngargon <98019023+cyngargon@users.noreply.github.com>
2022-04-05 12:22:13 +0200

    Comentaris

--- Personatge/src/personatge/Personatge.java
+++ Personatge/src/personatge/Personatge.java
@@ -1,40 +1,97 @@
 package personatge;
 
-public abstract class Personatge {
-
+public abstract class Personatge extends Tabler{
+    //Attributes
 	private int vides;
-	private int[][]posicio;
 	private int monedes;
+        private boolean clau;
 	
+    //Constructors
+        public Personatge(){
+        }
+        public Personatge(int vides, int monedes, boolean clau) {
+            this.vides = vides;
+            this.monedes = monedes;
+            this.clau = clau;
+        }
+        
+    //Getter and setter
+        public int getVides() {
+            return vides;
+        }
+
+        public void setVides(int vides) {
+            this.vides = vides;
+        }
+
+        public int getMonedes() {
+            return monedes;
+        }
+
+        public void setMonedes(int monedes) {
+            this.monedes = monedes;
+        }
+        
+        public boolean isClau() {
+            return clau;
+        }
+
+        public void setClau(boolean clau) {
+            this.clau = clau;
+        }
+        
+        
+    //Methods
 	public abstract String missatgePosicio();
-	
-	public int mostrarMenu() {
-		int seleccio = 0;
-		return seleccio;
+        public abstract void moure();
+        public abstract void batalla();
+        
+	public void mostrarMenu() {
+            int [] posicio = super.getPosicioPersonatge();
+            int [][] tauler = super.getTauler();
+            int numero = tauler[posicio[0]][posicio[1]];
+            char respuesta;
+            switch (numero){
+                case 0:
+                    System.out.println("M: Moverse" + 
+                                        "\nC: Cambiar personaje" +
+                                        "\nS: Salir del juego");
+                    respuesta = Teclat.llegirChar();
+                    //Cridar funció
+                    break;
+                case 1:
+                    System.out.println("B: Batalla" +
+                                        "\nM: Moverse" +
+                                        "\nC: Cambiar personaje" +
+                                        "\nS: Salir del juego");
+                    respuesta = Teclat.llegirChar();
+                    //Cridar funcio
+                    break;
+                case 3: case 2:
+                    System.out.println("M: Moverse" + 
+                                        "\nR: Recoger" +
+                                        "\nC: Cambiar personaje" +
+                                        "\nS: Salir del juego");
+                    respuesta = Teclat.llegirChar();
+                    //Cridar funcio
+                    break;
+                default:
+                    System.out.println("ERROR!");
+            }
 	}
-	
 	public String mostrarPosicio() {
 		return	"W -> Arriba"
 				+ "S -> Abajo"
 				+ "D -> Derecha"
 				+ "A -> Izquierda";
 	}
-	
 	public void checkCasilla() {
 		
 	}
-	
 	public void checkPosicio() {
 		
 	}
-	
 	public void opcionesMenu() {
-		int seleccio = mostrarMenu();
-		switch (seleccio) {
-			case 1:
-				System.out.println("Moverse");
-				missatgePosicio();
-				
-		}
+            
 	}
 }
--- Personatge/src/personatge/Tabler.java
+++ Personatge/src/personatge/Tabler.java
@@ -1,19 +1,24 @@
 package personatge;
 
 public class Tabler {
+    //0 --> RES
+	//1 --> ENEMIC
+	//2 --> MONEDA
+	//3 --> CLAU
+    //Attributes
 	private int[][] tauler;
 	private int[] posicioPersonatge;
-
+        private final int RES = 0;
+	private final int ENEMIC = 1;
+	private final int MONEDA = 2;
+	private final int CLAU = 3;
+    
+    //Constructors
 	public Tabler() {
-		this.posicioPersonatge = new int [2]; 
+		this.posicioPersonatge = new int [] {0, 0};
 	}
-	
-	public Tabler(int[][] tauler, int[] posicioPersonatge) {
-		this();
-		this.tauler = tauler;
-		this.posicioPersonatge = posicioPersonatge;
-	}
-
+        
+    //Getter and setter
 	public int[][] getTauler() {
 		return tauler;
 	}
@@ -30,45 +35,53 @@ public class Tabler {
 		this.posicioPersonatge = posicioPersonatge;
 	}
 	
-	
+    //Methods
 	public void iniciPrograma() {
 		int [] dimensions = new int [2];
+		boolean checkDimension;
 		//Condicional 5 al 20
-		System.out.print("Introduce las columnas del mapa: ");
-		dimensions[0] = Teclat.llegirInt();
-		System.out.print("Introduce las filas del mapa: ");
-		dimensions[1] = Teclat.llegirInt();
+		do{
+			System.out.print("Introduce las columnas del mapa: ");
+			dimensions[0] = Teclat.llegirInt();
+			System.out.print("Introduce las filas del mapa: ");
+			dimensions[1] = Teclat.llegirInt();
+			if (dimensions[0] < 5 || dimensions[0] > 20 || dimensions[1] < 5 || dimensions[1] > 20) {
+				System.out.println("No se pueden crear este número de filas i columnas. Intentalo de nuevo...");
+				checkDimension = false;
+			} else {
+				System.out.println("Creando mapa...");
+				checkDimension = true;
+			}
+		}while(checkDimension == false);
 		tauler = new int[dimensions[0]][dimensions[1]];
 	}
 	
 	public void generarTauler() {		
+		int filaClau, columnaClau;
 		iniciPrograma();
 		for (int i = 0; i < tauler.length; i++) {
 			for (int j = 0; j < tauler[0].length; j++) {
 				tauler[i][j] = (int) (Math.random()*3);
 			}
 		}
-		/*
-		for (int i = 0; i < 2; i++) {
-			tauler[i][j] = (int) (Math.random()*3);
-		}
-		*/
+		filaClau = (int) (Math.random()*tauler.length);
+		columnaClau = (int) (Math.random()*tauler[0].length);
+		tauler [filaClau][columnaClau] = CLAU;
 	}
 	
 	public void mostrarTauler(){
-		
-		for (int i = 0; i < tauler.length; i++) {
-			for (int j = 0; j < tauler[0].length; j++) {
-				if (i == posicioPersonatge[0] && j == posicioPersonatge[1]) {
-					System.out.print("[x]");
-				} else if (i == tauler.length - 1 && j == tauler[0].length - 1) {
-					System.out.print("[SALIDA]");
-				}
-				else {
-					System.out.print("[ ]");
-				}
-			}
-			System.out.println();
+            for (int i = 0; i < tauler.length; i++) {
+                    for (int j = 0; j < tauler[0].length; j++) {
+                            if (i == posicioPersonatge[0] && j == posicioPersonatge[1]) {
+                                    System.out.print("[x]");
+                            } else if (i == tauler.length - 1 && j == tauler[0].length - 1) {
+                                    System.out.print("[SALIDA]");
+                            }
+                            else {
+                                    System.out.print("[ ]");
+                            }
+                    }
+                    System.out.println();
 		}
 	}
 	
