ee400322ce195888205e30958e703bef19edc33d
cyngargon <98019023+cyngargon@users.noreply.github.com>
2022-04-05 12:03:03 +0200

    Junto lo del Pol i lo meu

--- Personatge/src/personatge/Guerrer.java
+++ Personatge/src/personatge/Guerrer.java
@@ -2,10 +2,19 @@ package personatge;
 
 public class Guerrer extends Personatge {
 
-	@Override
-	public String missatgePosicio() {
-		
-		return "Et pots moures d'1 en 1" + super.mostrarPosicio();
-	}
+    @Override
+    public String missatgePosicio() {
+            return "Et pots moures d'1 en 1" + super.mostrarPosicio();
+    }
+
+    @Override
+    public void moure() {
+        
+    }
+
+    @Override
+    public void batalla() {
+        
+    }
 	
 }
