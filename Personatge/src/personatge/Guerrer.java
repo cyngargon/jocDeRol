6e99376dbb2d5b25dfd18431575a76e55335b5f3
cyngargon <cgarciag2122@politecnics.barcelona>
2022-04-19 12:11:06 +0200

    Movimiento acabado pero no chequeado

--- Personatge/src/personatge/Guerrer.java
+++ Personatge/src/personatge/Guerrer.java
@@ -10,13 +10,29 @@ public class Guerrer extends Personatge {
     @Override
     public void moure(char [] direccio) {
         int [] posicio;
+        boolean correcte;
         posicio = super.getPosicioPersonatge();
         
         for (int i = 0; i < direccio.length; i++) {
             switch (direccio[i]){
-                case 'W':
-                    super.checkMoviment(direccio[i]);
-                    posicio[0]++;
+                case 'W': case 'S':
+                    correcte = super.checkMoviment(direccio[i]);
+                    if(correcte==true){
+                        posicio[0]++;
+                        super.setPosicioPersonatge(posicio);
+                    }else{
+                        System.out.println("Error. No te puedes mover");
+                    }
+                    break;
+                case 'A': case 'D':
+                    correcte = super.checkMoviment(direccio[i]);
+                    if(correcte==true){
+                        posicio[1]++;
+                        super.setPosicioPersonatge(posicio);
+                    }else{
+                        System.out.println("Error. No te puedes mover");
+                    }
+                    break;
             }
         }
     }
