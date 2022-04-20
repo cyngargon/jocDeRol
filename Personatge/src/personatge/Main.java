0e716a62402b0683e386a4f62005aaac8c6c0f00
cyngargon <cgarciag2122@politecnics.barcelona>
2022-04-20 09:07:41 +0200

    Avançat el tema de moviment. Queda mirar errors.

--- Personatge/src/personatge/Main.java
+++ Personatge/src/personatge/Main.java
@@ -3,9 +3,10 @@ package personatge;
 public class Main {
         
 	public static void main(String[] args) {
-            
-		Personatge p1 = new Guerrer();
-		
-                p1.mostrarDireccio();
+		//Personatge p1 = new Guerrer();
+                //p1.programa();
+                Personatge p2 = new Mag();
+                p2.programa();
+                
 	}
 }
