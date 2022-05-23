8627612b42f7063f8bd5b0a7751cfa6bbadd9870
PolCrespo5 <98875894+PolCrespo5@users.noreply.github.com>
2022-05-23 10:40:40 +0200

    BASE DE DADES SCRIPT AMB TAULA (dades_partida)

--- jocrol.sql
+++ jocrol.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS jocrol;
 CREATE DATABASE jocrol;
 USE jocrol;
 
+CREATE TABLE if not exists DADES_PARTIDA (
+id_partida INT AUTO_INCREMENT,
+nom VARCHAR(3), 
+puntuacio INT,
+tempsJugat INT,
+PRIMARY KEY (id_partida)
+);
+
+SELECT * FROM DADES_PARTIDA;
